f9ac36741212eeaf70427f1cf9b6ddc22532d8dc
frmnxhken <frmnxhken@gmail.com>
2026-04-17 10:49:53 +0700

    Update Logic NN (SLP) add evaluation data test

--- NN/NN.js
+++ NN/NN.js
@@ -1,10 +1,10 @@
-import { x, y } from "./data.js";
+import { x_train, y_train, x_test, y_test } from "./data.js";
 
 class Neural {
   constructor() {
     this.lr = 0.1;
     this.epoch = 10;
-    this.weights = [0.1, 0.1, 0.1];
+    this.weights = [];
     this.predicts = [];
   }
 
@@ -21,7 +21,25 @@ class Neural {
     }
   }
 
+  predict(x_test) {
+    for (let i = 0; i < x_test.length; i++) {
+      let z = 0;
+      for (let j = 0; j < x_test[0].length; j++) {
+        let current = x_test[i][j];
+        let net = this.weights[j] * current;
+        z += net;
+      }
+      let y_pred = this.activation(z);
+      this.predicts.push(y_pred);
+      z = 0;
+    }
+    return this.predicts;
+  }
+
   fit(x, y) {
+    for (let w = 0; w < x[0].length; w++) {
+      this.weights.push(0);
+    }
     for (let e = 0; e < this.epoch; e++) {
       for (let i = 0; i < x.length; i++) {
         let z = 0;
@@ -32,7 +50,6 @@ class Neural {
         }
         let y_pred = this.activation(z);
         let error = y[i] - y_pred;
-        this.predicts.push(y_pred);
         this.updateWeight(error, x[i]);
         z = 0;
       }
@@ -56,10 +73,18 @@ class Evaluation {
   }
 
   matrix() {
-    this.tp = this.y_pred.filter((yp, i) => yp === y[i] && yp === 1).length;
-    this.tn = this.y_pred.filter((yp, i) => yp === y[i] && yp === 0).length;
-    this.fp = this.y_pred.filter((yp, i) => yp !== y[i] && y === 1).length;
-    this.fn = this.y_pred.filter((yp, i) => yp !== y[i] && y === 0).length;
+    this.tp = this.y_pred.filter(
+      (yp, i) => yp === this.y[i] && yp === 1,
+    ).length;
+    this.tn = this.y_pred.filter(
+      (yp, i) => yp === this.y[i] && yp === 0,
+    ).length;
+    this.fp = this.y_pred.filter(
+      (yp, i) => yp !== this.y[i] && this.y[i] === 1,
+    ).length;
+    this.fn = this.y_pred.filter(
+      (yp, i) => yp !== this.y[i] && this.y[i] === 0,
+    ).length;
   }
 
   confusion_matrix() {
@@ -78,8 +103,8 @@ class Evaluation {
   }
 }
 const nn = new Neural();
-nn.fit(x, y);
-const y_pred = nn.predicts;
-const evaluation = new Evaluation(y, y_pred.slice(-y.length));
+nn.fit(x_train, y_train);
+const y_pred = nn.predict(x_test);
+const evaluation = new Evaluation(y_test, y_pred);
 evaluation.confusion_matrix();
 evaluation.report_classification();
